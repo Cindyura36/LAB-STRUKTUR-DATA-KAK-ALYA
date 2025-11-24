635513c7a3eb011b27f169f15fab8e906b720191
Cindyura36 <cindyaulia4462@gmail.com>
2025-11-24 16:02:12 +0700

    Pertemuan 3 : Sorting

---
+++ pertemuan 3/insertion.cc
@@ -0,0 +1,24 @@
+// Program Insertion Sort
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Masukkan jumlah elemen data: ";
+    cin >> n;
+
+    int data[n];
+    cout << "Masukkan elemen-elemen data: \n";
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+    }
+
+    // Insertion Sort dari terkecil hingga terbesar
+    
+    cout << "Data setelah diurutkan dari terkecil hingga terbesar:\n";
+    for (int i = 0; i < n; i++) {
+        cout << data[i] << " ";
+    }
+    
+    cout << endl;
+    return 0;
+}
